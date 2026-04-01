d897369c0ce216284650e9ba0f426d8f7a3b3888
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-01 09:15:39 +0545

    added how to get id from user and send message to that user

---
+++ index.js
@@ -0,0 +1,31 @@
+const { WebClient } = require("@slack/web-api");
+const client = new WebClient("your-but-user-oauth-token");
+
+async function sendMessage() {
+  await client.chat.postMessage({
+    channel: "channel-id",
+    text: "hello kshitij",
+  });
+}
+
+sendMessage();
+
+async function findUserByEmail(emailAddress) {
+  try {
+    const result = await client.users.lookupByEmail({
+      email: emailAddress,
+    });
+
+    if (result.ok) {
+      console.log(`User ID for ${emailAddress} is: ${result.user.id}`);
+      return result.user.id;
+    }
+  } catch (error) {
+    if (error.data && error.data.error === "users_not_found") {
+      console.error("No user found with that email address.");
+    } else {
+      console.error("Error looking up user:", error);
+    }
+  }
+}
+findUserByEmail("kshitiz.khatri@codavatar.tech");
